a7221053790ec52503eb8d676ebb8e2fbec8135c
hwglee <hung1306.lth@gmail.com>
2025-04-12 12:57:30 +0700

    update refactor orderfragment

---
+++ app/src/main/java/com/project/clothingstore/view/fragment/OrderFragment.java
@@ -0,0 +1,10 @@
+package com.project.clothingstore.view.fragment;
+
+import androidx.fragment.app.Fragment;
+
+public class OrderFragment extends Fragment {
+
+    public OrderFragment() {
+        // Required empty public constructor
+    }
+}
